7a3794c8c909ba81fc42679f0aecab47b4dea576
NataRich <natarichjay@gmail.com>
2022-04-19 20:08:20 -0500

    Complete parser

--- data_models.py
+++ data_models.py
@@ -1,3 +1,35 @@
+import os
+from re import sub
+
+
+columnSeparator = "|"
+
+# Dictionary of months used for date transformation
+MONTHS = {'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06',\
+        'Jul':'07','Aug':'08','Sep':'09','Oct':'10','Nov':'11','Dec':'12'}
+
+
+def transformMonth(mon):
+    if mon in MONTHS:
+        return MONTHS[mon]
+    else:
+        return mon
+
+
+def transformDttm(dttm):
+    dttm = dttm.strip().split(' ')
+    dt = dttm[0].split('-')
+    date = '20' + dt[2] + '-'
+    date += transformMonth(dt[0]) + '-' + dt[1]
+    return date + ' ' + dttm[1]
+
+
+def transformDollar(money):
+    if money == None or len(money) == 0:
+        return money
+    return sub(r'[^\d.]', '', money)
+
+
 def saferstr(string):
     """This is a util function that makes a normal string safer.
 
@@ -20,6 +52,11 @@ def saferstr(string):
     return '"' + string.replace('"', '""') + '"'
 
 
+safernull = '"NULL"'
+reportdir = os.path.join(os.getcwd(), 'report')
+resultdir = os.path.join(os.getcwd(), 'result')
+
+
 class CategoryData:
     _counter = 0
     _ln = '{}|{}'
@@ -37,17 +74,33 @@ class CategoryData:
         return uid
 
     def add_if_absent(self, name):
-        if name is None:
-            raise ValueError('DEBUG: The category\'s name must not be null')
+        assert name is not None
 
         safername = saferstr(name)
-        if safername not in self.names:
-            self.uids.append(self.next_uid())
-            self.names.append(safername)
+        for i in range(len(self.names)):
+            if self.names[i] == safername:
+                return self.uids[i]
+
+        uid = self.next_uid()
+        self.uids.append(uid)
+        self.names.append(safername)
+        
+        return uid
 
     def lineify(self):
         return [CategoryData._ln.format(uid, name) 
                 for uid, name in zip(self.uids, self.names)]
+
+    def flush(self, file):
+        target = os.path.join(resultdir, file)
+
+        with open(target, 'w+') as f:
+            for i in range(len(self.uids)):
+                ln = CategoryData._ln.format(self.uids[i], self.names[i])
+                f.write(f'{ln}\n')
+
+        self.uids  = []
+        self.names = []
 
 
 class UserData:
@@ -62,26 +115,46 @@ class UserData:
     def __str__(self):
         return str(self.lineify())
 
-    def add_if_absent(self, uid, rating, location, country):
-        if uid is None:
-            raise ValueError('DEBUG: The user\'s id must not be null')
+    def smart_add(self, uid, rating, location, country):
+        assert uid is not None
 
-        if rating is None:
-            raise ValueError('DEBUG: The user\'s rating must not be null')
+        saferid = saferstr(uid)
+        for i in range(len(self.uids)):
+            if self.uids[i] == saferid:
+                if self.ratings[i] == safernull and rating is not None:
+                    self.ratings[i] = rating
+                if self.locations[i] == safernull and location is not None:
+                    self.locations[i] = saferstr(location)
+                if self.countries[i] == safernull and country is not None:
+                    self.countries[i] = saferstr(country)
 
-        if uid not in self.uids:
-            saferloc = saferstr(location if location is not None else 'NULL')
-            safercntry = saferstr(country if country is not None else 'NULL')
+        loc = saferstr(location) if location is not None else safernull
+        cnt = saferstr(country) if country is not None else safernull
+        rat = rating if rating is not None else safernull
 
-            self.uids.append(saferstr(uid))
-            self.ratings.append(rating)
-            self.locations.append(saferloc)
-            self.countries.append(safercntry)
+        self.uids.append(saferid)
+        self.ratings.append(rat)
+        self.locations.append(loc)
+        self.countries.append(cnt)
 
     def lineify(self):
         return [UserData._ln.format(uid, rating, location, country)
                 for uid, rating, location, country
                 in zip(self.uids, self.ratings, self.locations, self.countries)]
+
+    def flush(self, file):
+        target = os.path.join(resultdir, file)
+
+        with open(target, 'w+') as f:
+            for i in range(len(self.uids)):
+                ln = UserData._ln.format(self.uids[i], self.ratings[i],
+                        self.locations[i], self.countries[i])
+                f.write(f'{ln}\n')
+
+        self.uids      = []
+        self.ratings   = []
+        self.locations = []
+        self.countries = []
 
 
 class BidData:
@@ -104,29 +177,42 @@ class BidData:
         return uid
 
     def add(self, user_id, item_id, time, amount):
-        if user_id is None:
-            raise ValueError('DEBUG: The bidder id must not be null')
+        assert user_id is not None
+        assert item_id is not None
 
-        if item_id is None:
-            raise ValueError('DEBUG: The bidding item id must not be null')
+        saferuserid = saferstr(user_id)
+        tm = saferstr(time) if time is not None else safernull
+        am = saferstr(amount) if amount is not None else safernull
 
-        if time is None:
-            raise ValueError('DEBUG: The bid time must not be null')
-
-        if amount is None:
-            raise ValueError('DEBUG: The bid amount must not be null')
-
-        self.uids.append(self.next_uid())
-        self.user_ids.append(saferstr(user_id))
+        uid = self.next_uid()
+        self.uids.append(uid)
+        self.user_ids.append(saferuserid)
         self.item_ids.append(item_id)
-        self.times.append(saferstr(time))
-        self.amounts.append(saferstr(amount))
+        self.times.append(tm)
+        self.amounts.append(am)
+
+        return uid
 
     def lineify(self):
         return [BidData._ln.format(uid, user_id, item_id, time, amount)
                 for uid, user_id, item_id, time, amount
                 in zip(self.uids, self.user_ids, self.item_ids, self.times,
                     self.amounts)]
+
+    def flush(self, file):
+        target = os.path.join(resultdir, file)
+
+        with open(target, 'w+') as f:
+            for i in range(len(self.uids)):
+                ln = BidData._ln.format(self.uids[i], self.user_ids[i],
+                        self.item_ids[i], self.times[i], self.amounts[i])
+                f.write(f'{ln}\n')
+
+        self.uids     = []
+        self.user_ids = []
+        self.item_ids = []
+        self.times    = []
+        self.amounts  = []
 
 
 class CategorizationData:
@@ -146,25 +232,38 @@ class CategorizationData:
         CategorizationData._counter += 1
         return uid
 
-    def add(self, item_id, cate_id):
-        if item_id is None:
-            raise ValueError('DEBUG: The item id must not be null')
+    def add_if_absent(self, item_id, cate_id):
+        assert item_id is not None
+        assert cate_id is not None
 
-        if cate_id is None:
-            raise ValueError('DEBUG: The category id must not be null')
+        for i in range(len(self.item_ids)):
+            if self.item_ids[i] == item_id and self.cate_ids[i] == cate_id:
+                return self.uids[i]
 
-        cates = [self.cate_ids[i] for i in range(len(self.item_ids))
-                if self.item_ids[i] == item_id]
-        if cate_id in cates:
-            return
-
+        uid = self.next_uid()
+        self.uids.append(uid)
         self.item_ids.append(item_id)
         self.cate_ids.append(cate_id)
+
+        return uid
 
     def lineify(self):
         return [CategorizationData._ln.format(uid, item_id, cate_id)
                 for uid, item_id, cate_id 
                 in zip(self.uids, self.item_ids, self.cate_ids)]
+
+    def flush(self, file):
+        target = os.path.join(resultdir, file)
+
+        with open(target, 'w+') as f:
+            for i in range(len(self.uids)):
+                ln = CategorizationData._ln.format(self.uids[i],
+                        self.item_ids[i], self.cate_ids[i])
+                f.write(f'{ln}\n')
+
+        self.uids     = []
+        self.item_ids = []
+        self.cate_ids = []
 
 
 class ItemData:
@@ -179,57 +278,212 @@ class ItemData:
         self.first_bids = []
         self.num_bidss  = []
         self.starteds   = []
-        self.ends       = []
+        self.endss      = []
         self.descs      = []
 
     def __str__(self):
         return str(self.lineify())
 
-    def add_if_absent(self, uid, seller_id, name, currently, buy_price,
-            first_bid, num_bids, started, end, desc):
-        if uid is None:
-            raise ValueError('DEBUG: The item id must not be null')
+    def add(self, uid, seller_id, name, currently, buy_price, first_bid,
+            num_bids, started, ends, desc):
+        assert uid is not None
+        assert uid not in self.uids
+        assert seller_id is not None
 
-        if seller_id is None:
-            raise ValueError('DEBUG: The item\'s seller id must not be null')
+        nm = saferstr(name) if name is not None else safernull
+        cur = saferstr(currently) if currently is not None else safernull
+        buy = saferstr(buy_price) if buy_price is not None else safernull
+        fb = saferstr(first_bid) if first_bid is not None else safernull
+        nb = saferstr(num_bids) if num_bids is not None else safernull
+        st = saferstr(started) if started is not None else safernull
+        ed = saferstr(ends) if ends is not None else safernull
+        de = saferstr(desc) if desc is not None else safernull
 
-        if name is None:
-            raise ValueError('DEBUG: The item name must not be null')
-
-        if currently is None:
-            raise ValueError('DEBUG: The item currently must not be null')
-
-        if first_bid is None:
-            raise ValueError('DEBUG: The item first bid must not be null')
-
-        if num_bids is None:
-            raise ValueError('DEBUG: The item number of bids must not be null')
-
-        if started is None:
-            raise ValueError('DEBUG: The item start time must not be null')
-
-        if end is None:
-            raise ValueError('DEBUG: The item end time must not be null')
-
-        if uid not in self.uids:
-            saferbp = saferstr(buy_price if buy_price is not None else 'NULL')
-
-            self.uids.append(uid)
-            self.seller_ids.append(saferstr(seller_id))
-            self.names.append(saferstr(name))
-            self.currentlys.append(saferstr(currently))
-            self.buy_prices.append(saferbp)
-            self.first_bids.append(saferstr(first_bid))
-            self.num_bidss.append(saferstr(num_bids))
-            self.starteds.append(saferstr(started))
-            self.ends.append(saferstr(end))
-            self.descs.append(saferstr(desc))
+        self.uids.append(uid)
+        self.seller_ids.append(saferstr(seller_id))
+        self.names.append(nm)
+        self.currentlys.append(cur)
+        self.buy_prices.append(buy)
+        self.first_bids.append(fb)
+        self.num_bidss.append(nb)
+        self.starteds.append(st)
+        self.endss.append(ed)
+        self.descs.append(de)
     
     def lineify(self):
         return [ItemData._ln.format(uid, seller_id, name, currently, buy_price,
-                                    first_bid, num_bids, started, end, desc)
+                                    first_bid, num_bids, started, ends, desc)
                 for uid, seller_id, name, currently, buy_price, first_bid,
-                    num_bids, started, end, desc
+                    num_bids, started, ends, desc
                 in zip(self.uids, self.seller_ids, self.names, self.currentlys,
                     self.buy_prices, self.first_bids, self.num_bidss,
-                    self.starteds, self.ends, self.descs)]
+                    self.starteds, self.endss, self.descs)]
+
+    def flush(self, file):
+        target = os.path.join(resultdir, file)
+
+        with open(target, 'w+') as f:
+            for i in range(len(self.uids)):
+                ln = ItemData._ln.format(self.uids[i], self.seller_ids[i],
+                        self.names[i], self.currentlys[i], self.buy_prices[i],
+                        self.first_bids[i], self.num_bidss[i],
+                        self.starteds[i], self.endss[i], self.descs[i])
+                f.write(f'{ln}\n')
+
+        self.uids       = []
+        self.seller_ids = []
+        self.names      = []
+        self.currentlys = []
+        self.buy_prices = []
+        self.first_bids = []
+        self.num_bidss  = []
+        self.starteds   = []
+        self.endss      = []
+        self.descs      = []
+
+
+class Parser:
+    def __init__(self):
+        self.c = CategoryData()
+        self.u = UserData()
+        self.b = BidData()
+        self.i = ItemData()
+        self.ci = CategorizationData()
+        
+        self.nullmap = {}
+        self.logs = []
+        self.f = ''
+        self.fn = ''
+
+    def _log_if_none(self, key, value, mark):
+        assert key is not None
+        assert mark is not None
+
+        if value is None:
+            log = f'{mark}'
+            if key not in self.nullmap:
+                self.nullmap[key] = [log]
+            else:
+                self.nullmap[key].append(log)
+        return value
+
+    def _log_none(self, key, mark):
+        log = f'Unexpected null found in {key} of {mark}'
+        self.logs.append(log)
+
+    def _get(self, d, k, mark, key=None):
+        return self._log_if_none(key or k, d[k] if k in d else None, mark)
+
+    def _parse_category(self, item, item_id):
+        assert item is not None
+        assert item_id is not None
+
+        categories = self._get(item, 'Category', item_id)
+        if categories is not None:
+            for category in categories:
+                cate_id = self.c.add_if_absent(category)
+                self.ci.add_if_absent(item_id, cate_id)
+
+    def _parse_bid(self, item, item_id):
+        assert item is not None
+        assert item_id is not None
+
+        bids = self._get(item, 'Bids', item_id)
+        if bids is None:
+            return
+
+        for e in bids:
+            if e is None:
+                self._log_none('Bids', item_id)
+                continue
+
+            bid = self._get(e, 'Bid', item_id)
+            if bid is None:
+                self._log_none('Bid of Bids', item_id)
+                continue
+
+            bdr = self._get(bid, 'Bidder', item_id)
+            if bdr is None:
+                self._log_none('Bidder of Bids', item_id)
+                continue
+
+            user_id = self._get(bdr, 'UserID', item_id, key='BidderID')
+            rating = self._get(bdr, 'Rating', item_id, key='BidderRating')
+            location = self._get(bdr, 'Location', item_id, key='BidderLocation')
+            country = self._get(bdr, 'Country', item_id, key='BidderCountry')
+            self.u.smart_add(user_id, rating, location, country)
+
+            time = transformDttm(self._get(bid, 'Time', item_id, key='BidTime'))
+            amount = transformDollar(self._get(bid, 'Amount', item_id, key='BidAmount'))
+            self.b.add(user_id, item_id, time, amount)  # TODO: transform
+
+    def _parse_item(self, item, item_id):
+        assert item is not None
+        assert item_id is not None
+
+        seller = self._get(item, 'Seller', item_id)
+        if seller is None:
+            self._log_none('Seller', item_id)
+            return
+        
+        user_id = self._get(seller, 'UserID', item_id, key='SellerID')
+        rating = self._get(seller, 'Rating', item_id, key='SellerRating')
+        location = self._get(item, 'Location', item_id)
+        country = self._get(item, 'Country', item_id)
+        self.u.smart_add(user_id, rating, location, country)
+
+        name = self._get(item, 'Name', item_id)
+        currently = transformDollar(self._get(item, 'Currently', item_id))
+        buy_price = transformDollar(self._get(item, 'Buy_Price', item_id))
+        first_bid = transformDollar(self._get(item, 'First_Bid', item_id))
+        num_bids = self._get(item, 'Num_Bids', item_id)
+        started = transformDttm(self._get(item, 'Started', item_id))
+        ends = transformDttm(self._get(item, 'Ends', item_id))
+        desc = self._get(item, 'Description', item_id)
+        self.i.add(item_id, user_id, name, currently, buy_price, first_bid,
+                num_bids, started, ends, desc)
+
+    def set_file(self, file):
+        self.f = os.path.basename(file)
+        self.fn = self.f.split('.')[0]
+
+    def parse(self, items):
+        assert items is not None
+
+        for item in items:
+            item_id = self._get(item, 'ItemID', 'at start')
+            assert item_id is not None
+
+            self._parse_category(item, item_id)
+            self._parse_bid(item, item_id)
+            self._parse_item(item, item_id)
+
+    def flush(self):
+        self.c.flush('category.dat')
+        self.u.flush('user.dat')
+        self.b.flush('bid.dat')
+        self.i.flush('item.dat')
+        self.ci.flush('categorization.dat')
+
+
+    def report(self):
+        target_null = os.path.join(reportdir, f'{self.fn}_nullmap.txt')
+        target_log = os.path.join(reportdir, f'{self.fn}_log.txt')
+        
+        with open(target_null, 'w+') as f:
+            f.write(f'Null Map ({self.fn}):\n\n')
+            f.write(f'These keys were null when parsing {self.f}:\n')
+            for k in self.nullmap:
+                f.write(f'  {k}\n')
+            f.write('\nDetails see below (numbers are ItemIDs):\n')
+            for k in self.nullmap:
+                f.write(f'{k} was null at\n')
+                for ln in self.nullmap[k]:
+                    f.write(f'    {ln}\n')
+        self.nullmap = {}
+
+        with open(target_log, 'w+') as f:
+            f.write('Null Log:\n\n')
+            for ln in self.logs:
+                f.write(f'{fn}\n')
+        self.logs = []
